bd9c06ee2dc620ed870aecaee9a1d0b1c7b9f259
awesomealexs <awesomealexs@yandex.ru>
2024-02-08 00:49:01 +0300

    initial

---
+++ asdf.php
@@ -0,0 +1,292 @@
+<?php
+
+$text = <<<'HTML'
+<section class="include">
+                        <div class="card-info-block">
+                            <div class="card-info-item">
+                                <p class="info__title">Основная информация</p>
+                                <div class="info-part">
+                                    <p class="info__name">Реестровый номер</p>
+                                    <p class="info__text">
+                                        РТО 025233                                    </p>
+                                </div>
+                                <div class="info-part">
+                                    <p class="info__name">Полное наименование</p>
+                                    <p class="info__text">
+                                        Автономная некоммерческая организация "Дирекция по организации и проведению социально-значимых программ"                                    </p>
+                                </div>
+                                <div class="info-part">
+                                    <p class="info__name">Сокращенное наименование</p>
+                                    <p class="info__text">
+                                        АНО "Дирекция по организации и проведению социально-значимых программ"                                    </p>
+                                </div>
+                                <div class="info-part">
+                                    <p class="info__name">Адрес</p>
+                                    <p class="info__text">
+                                        196140, г. Санкт-Петербург, п. Шушары, ш. Петербургское, д. 64, к. 1, литер А, ч.п. 1252                                    </p>
+                                </div>
+                                <div class="info-part">
+                                    <p class="info__name">ИНН</p>
+                                    <p class="info__text">
+                                        7820071675                                    </p>
+                                </div>
+                                <div class="info-part">
+                                    <p class="info__name">ОГРН</p>
+                                    <p class="info__text">
+                                        1197800004709                                    </p>
+                                </div>
+                                <div class="info-part">
+                                    <p class="info__name">Адреса обособленных подразделений туроператора, осуществляющих
+                                        туроператорскую деятельность</p>
+                                    <p class="info__text">
+                                                                                                                        нет
+                                                                            </p>
+                                </div>
+                                                                    <div class="info-part">
+                                        <p class="info__name">Сайт</p>
+                                        <p class="info__text">
+                                            <a target="_blank" href="https://www.ossp.spb.ru">www.ossp.spb.ru</a>
+                                        </p>
+                                    </div>
+                                                            </div>
+                                                                                        <div class="card-info-item">
+                                    <p class="info__title">Финансовое обеспечение</p>
+                                    <div class="info-part">
+                                        <p class="info__name">Общий размер финансового обеспечения:</p>
+                                        <p class="info__text">
+                                            500000                                         </p>
+                                    </div>
+
+
+
+
+    <div class="info-part">
+        <p class="info__name">Сфера туроператорской деятельности:</p>
+        <p class="info__text">
+                            внутренний туризм<br>
+                            международный въездной<br>
+                    </p>
+    </div>
+<div class="info-part">
+    <p class="info__name">Способ финансового обеспечения:</p>
+    <p class="info__text">договор страхования гражданской ответственности туроператора</p>
+</div>
+    <div class="info-part">
+        <p class="info__name">Размер финансового обеспечения:</p>
+        <p class="info__text">500000</p>
+    </div>
+    <div class="info-part">
+        <p class="info__name">Документ:</p>
+        <p class="info__text">№ 0623 GL 000332 F от 05/09/2023</p>
+    </div>
+    <div class="info-part">
+        <p class="info__name">Срок действия финансового обеспечения:</p>
+        <p class="info__text">с 11/09/2023 по 10/09/2024</p>
+    </div>
+    <div class="info-part">
+        <p class="info__name">Наименование организации, предоставившей финансовое обеспечение:</p>
+        <p class="info__text">АО "Страховое общество газовой промышленности"</p>
+    </div>
+    <div class="info-part">
+        <p class="info__name">Адрес (место нахождения) организации, предоставившей финансовое обеспечение:</p>
+        <p class="info__text">107078,г Москва, Красносельский р-н, пр-кт Академика Сахарова, д 10</p>
+    </div>
+
+                                                        </div>
+                                                                                        <div class="card-info-item">
+                                    <p class="info__title">Сфера туроператорской деятельности</p>
+                                                                            <div class="info-part">
+                                            <p class="info__name">
+                                                внутренний туризм                                            </p>
+                                        </div>
+                                                                            <div class="info-part">
+                                            <p class="info__name">
+                                                международный въездной                                            </p>
+                                        </div>
+                                                                    </div>
+
+                            <div class="card-info-item" style="display: none">
+                                <p class="info__title">Дата и номер приказа Ростуризма о внесении сведений в единый федеральный реестр туроператоров</p>
+                                                                    <div class="info-part">
+                                        <p class="info__name">Номер приказа</p>
+                                        <p class="info__text">
+                                            707                                        </p>
+                                    </div>
+                                                                                                    <div class="info-part">
+                                        <p class="info__name">Дата приказа</p>
+                                        <p class="info__text">
+                                            2023-10-09                                        </p>
+                                    </div>
+                                                                                                    <div class="info-part">
+                                        <p class="info__name">Номер выданного свидетельства</p>
+                                        <p class="info__text">
+                                            101879                                        </p>
+                                    </div>
+                                                            </div>
+                        </div>
+                        <div class="options__bottom">
+                        <div class="card-link">
+                                                        <a href="/reestry/reestr-turoperatorov/" class="link__btn turoperator_btn">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M19 12L5 12" stroke="#1F5BFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path d="M12 5L5 12L12 19" stroke="#1F5BFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                                <p>Вернуться к списку</p>
+                            </a>
+                        </div>
+                        <div class="share-block">
+                            <a class="share-item" href="javascript:void(0)" onclick="window.print();">
+                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M4 7V1H14V7" stroke="#A2B3C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path d="M4 14H2.6C2.17565 14 1.76869 13.8361 1.46863 13.5444C1.16857 13.2527 1 12.857 1 12.4444V8.55556C1 8.143 1.16857 7.74734 1.46863 7.45561C1.76869 7.16389 2.17565 7 2.6 7H15.4C15.8243 7 16.2313 7.16389 16.5314 7.45561C16.8314 7.74734 17 8.143 17 8.55556V12.4444C17 12.857 16.8314 13.2527 16.5314 13.5444C16.2313 13.8361 15.8243 14 15.4 14H14" stroke="#A2B3C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path d="M14 11H4V17H14V11Z" stroke="#A2B3C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                                <div class="share-tip">
+                                    Печать
+                                </div>
+                            </a>
+                            <a class="share-item addToFavorite" href="javascript:void(0)" id="addToFavorite">
+                                <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M9 1L11.472 5.93691L17 6.73344L13 10.5741L13.944 16L9 13.4369L4.056 16L5 10.5741L1 6.73344L6.528 5.93691L9 1Z" stroke="#A2B3C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                                <div class="share-tip">
+                                    В избранное
+                                </div>
+                            </a>
+                            <div class="share-wrapper share-icon__link">
+    <a href="javascript:void(0)" class="share-item">
+        <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11 5C12.1046 5 13 4.10457 13 3C13 1.89543 12.1046 1 11 1C9.89543 1 9 1.89543 9 3C9 4.10457 9.89543 5 11 5Z" stroke="#A2B3C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M3 9.66699C4.10457 9.66699 5 8.77156 5 7.66699C5 6.56242 4.10457 5.66699 3 5.66699C1.89543 5.66699 1 6.56242 1 7.66699C1 8.77156 1.89543 9.66699 3 9.66699Z" stroke="#A2B3C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M11 14.334C12.1046 14.334 13 13.4386 13 12.334C13 11.2294 12.1046 10.334 11 10.334C9.89543 10.334 9 11.2294 9 12.334C9 13.4386 9.89543 14.334 11 14.334Z" stroke="#A2B3C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M4.72656 8.67383L9.2799 11.3272" stroke="#A2B3C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M9.27323 4.00684L4.72656 6.66017" stroke="#A2B3C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+        </svg>
+        <div class="share-tip">
+            Поделиться
+        </div>
+    </a>
+    <div class="share-inner" style="bottom: 58px;">
+        <ul class="share-list">
+            <li class="share-list__item" onclick="document.execCommand('copy');" data-url="https://tourism.gov.ru/reestry/reestr-turoperatorov/show.php" id="copyElement">
+                <a href="#/">
+                    <div class="icon-wrapper">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12.3454 4.44605C13.3066 3.50886 14.599 2.98917 15.9414 3.00005C17.2838 3.01289 18.5668 3.55521 19.5114 4.50904C20.4581 5.46419 20.9942 6.75132 21.0054 8.09605C21.0181 9.44107 20.505 10.7379 19.5754 11.71L19.5624 11.722L17.0974 14.207C16.3197 14.9944 15.3061 15.5062 14.2108 15.6647C13.1155 15.8232 11.9984 15.6196 11.0294 15.085C10.3859 14.7303 9.82618 14.2413 9.38842 13.651C9.23035 13.4381 9.16336 13.171 9.20218 12.9087C9.241 12.6463 9.38245 12.4101 9.59542 12.252C9.80839 12.094 10.0754 12.027 10.3378 12.0658C10.6002 12.1046 10.8364 12.2461 10.9944 12.459C11.2614 12.82 11.6024 13.118 11.9944 13.333C12.5829 13.6568 13.261 13.7798 13.9256 13.6832C14.5903 13.5867 15.2054 13.2759 15.6774 12.798L18.1364 10.32C18.7019 9.72559 19.0139 8.93446 19.0064 8.11405C18.9999 7.29055 18.6719 6.50219 18.0924 5.91704C17.5189 5.33758 16.7397 5.00802 15.9244 5.00005C15.1111 4.99331 14.3278 5.30732 13.7444 5.87405L12.3364 7.28505C12.2442 7.38062 12.134 7.45688 12.012 7.50937C11.89 7.56186 11.7588 7.58954 11.626 7.59079C11.4933 7.59203 11.3616 7.56683 11.2386 7.51663C11.1157 7.46644 11.004 7.39226 10.91 7.29844C10.8161 7.20461 10.7418 7.09301 10.6914 6.97016C10.641 6.8473 10.6156 6.71564 10.6167 6.58286C10.6177 6.45008 10.6452 6.31884 10.6976 6.1968C10.7499 6.07476 10.826 5.96436 10.9214 5.87205L12.3344 4.45604L12.3444 4.44605H12.3454Z" fill="#2A3338"></path>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8.73331 8.60504C9.42021 8.34677 10.1549 8.24038 10.8869 8.29321C11.6188 8.34603 12.3306 8.55681 12.9733 8.91104C13.6169 9.26569 14.1766 9.75478 14.6143 10.345C14.7724 10.558 14.8394 10.8251 14.8006 11.0874C14.7617 11.3498 14.6203 11.586 14.4073 11.744C14.1943 11.9021 13.9273 11.9691 13.6649 11.9303C13.4026 11.8915 13.1664 11.75 13.0083 11.537C12.7416 11.1773 12.4005 10.8792 12.0083 10.663C11.4199 10.3389 10.7418 10.2158 10.077 10.3124C9.41224 10.4089 8.79717 10.7199 8.32531 11.198L5.86531 13.676C5.29997 14.2709 4.98831 15.0624 4.99631 15.883C5.00331 16.709 5.33231 17.497 5.90931 18.079C6.48631 18.661 7.26531 18.989 8.07831 18.996C8.89081 19.0025 9.67321 18.6889 10.2563 18.123L11.6553 16.713C11.7478 16.6198 11.8578 16.5457 11.979 16.495C12.1001 16.4442 12.2301 16.4179 12.3614 16.4173C12.4928 16.4168 12.6229 16.4422 12.7445 16.492C12.866 16.5418 12.9766 16.615 13.0698 16.7075C13.163 16.8001 13.2372 16.91 13.2879 17.0312C13.3386 17.1523 13.365 17.2823 13.3655 17.4137C13.366 17.545 13.3407 17.6752 13.2909 17.7967C13.2411 17.9183 13.1678 18.0288 13.0753 18.122L11.6693 19.538L11.6573 19.55C10.6961 20.4872 9.40372 21.0069 8.06131 20.996C6.71874 20.9837 5.43534 20.4418 4.49031 19.488C3.54352 18.5326 3.00742 17.2451 2.99631 15.9C2.98405 14.5547 3.49792 13.2579 4.42831 12.286L4.44031 12.274L6.90531 9.78904C7.4224 9.2675 8.04591 8.86365 8.73331 8.60504Z" fill="#2A3338"></path>
+                        </svg>
+                    </div>
+                    <p>Копировать ссылку</p>
+                </a>
+            </li>
+                        <li class="share-list__item">
+                <a href="https://vk.com/share.php?url=https://tourism.gov.ru/reestry/reestr-turoperatorov/show.php" rel="nofollow noopener" target="_blank">
+                    <div class="icon-wrapper">
+                        <img src="/local/templates/.default/images/icons/share__vk.svg" alt="">
+                    </div>
+                    <p>Вконтакте</p>
+                </a>
+            </li>
+                        <li class="share-list__item">
+                <a href="https://connect.ok.ru/offer?url=https://tourism.gov.ru/reestry/reestr-turoperatorov/show.php" rel="nofollow noopener" target="_blank">
+                    <div class="icon-wrapper">
+                        <img src="/local/templates/.default/images/icons/share__odkl.svg" alt="">
+                    </div>
+                    <p>Одноклассники</p>
+                </a>
+            </li>
+                        <li class="share-list__item">
+                <a href="https://t.me/share/url?url=https://tourism.gov.ru/reestry/reestr-turoperatorov/show.php" rel="nofollow noopener" target="_blank">
+                    <div class="icon-wrapper">
+                        <img src="/local/templates/.default/images/icons/share_tlg.svg" alt="">
+                    </div>
+                    <p>Telegram</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>                        </div>
+                        </div>
+                    </section>';
+HTML;
+
+class asdf
+{
+    protected string $page;
+
+    public function __construct(string $page)
+    {
+        $this->page = $page;
+    }
+
+    public function getElementByCssSelector(string $selector)
+    {
+        $this->findByClass($selector);
+    }
+
+    protected function findByClass(string $className)
+    {
+        $baseRegexp = sprintf('/(?<item><[^>]*class="[^"]*%s[^"]*"[^>]*>)/', $className);
+
+        preg_match_all($baseRegexp, $this->page, $coreMatches);
+        preg_match('/<(?<tag>[^\s]*)\s[^>]+/', $coreMatches[0][0], $tagMatch);
+        $tag = $tagMatch['tag'];
+
+        foreach ($coreMatches[0] as $idx => $coreMatch) {
+            if ($idx > 0) {
+                $searchRegex = str_replace('(?<item>', '', $searchRegex);
+                $searchRegex = trim($searchRegex, ')');
+                $searchRegex = sprintf('%s.*?(?<item>%s)', $searchRegex, $coreMatch);
+            } else {
+                $searchRegex = $baseRegexp;
+            }
+            $resultedRegexp = $this->getItemRegexp($searchRegex, $tag);
+
+            preg_match($resultedRegexp, $this->page, $matchesResulted);
+            $regexp = $resultedRegexp;
+            if (0 === $idx) {
+                $searchRegex = $coreMatch;
+            }
+        }
+        $resultedRegexp = $this->getItemRegexp($regexp, $tag);
+
+        preg_match($resultedRegexp, $this->page, $matchesResulted);
+        $item = $matchesResulted['item'];
+        var_dump($item);
+
+        $a = 1;
+    }
+
+    protected function getItemRegexp(string $baseRegexp, string $tag): string
+    {
+        $temp = trim($baseRegexp, '/');
+        $temp = trim($temp, ')');
+        $regexp = sprintf('/%s.*?<\/%s>)/s', $temp, $tag);
+        var_dump($regexp);
+
+        preg_match($regexp, $this->page, $matches);
+
+        $countRegexp = sprintf('/(?<open>%s)|(?<close>\/%s)/', $tag, $tag);
+
+        preg_match_all($countRegexp, $matches[0], $matches12, \PREG_SET_ORDER);
+        $counter = 0;
+
+        array_map(static function ($item) use (&$counter) {
+            if ('' !== $item['open']) {
+                ++$counter;
+            } else {
+                --$counter;
+            }
+        }, $matches12);
+
+        if (0 !== $counter) {
+            $regexp = $this->getItemRegexp(trim($regexp, 's'), $tag);
+        }
+
+        return $regexp;
+    }
+}
+
+$m = new asdf($text);
+
+$m->getElementByCssSelector('card-info-item');
